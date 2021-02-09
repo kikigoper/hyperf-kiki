9dfa297ab461556fae42eef06d709d4aa7c89a5e
kai <522402295@qq.com>
2021-02-09 11:23:54 +0800

    第一次提交

---
+++ app/Controller/Admin/Zq.php
@@ -0,0 +1,58 @@
+<?php 
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Hyperf.plus
+ *
+ * @link     https://www.hyperf.plus
+ * @document https://doc.hyperf.plus
+ * @contact  4213509@qq.com
+ * @license  https://github.com/hyperf-plus/admin/blob/master/LICENSE
+ */
+
+namespace App\Controller\Admin;
+
+use App\Model\Zq as ModelZq;
+use HPlus\Admin\Controller\AbstractAdminController;
+use HPlus\Route\Annotation\AdminController;
+use HPlus\UI\Form;
+use HPlus\UI\Grid;
+
+/**
+ * @AdminController(prefix="zq", tag="", ignore=true))
+ */
+class Zq extends AbstractAdminController
+{
+	protected function grid()
+	{
+		$grid = new Grid(new ModelZq);
+		//$grid->hidePage(); 隐藏分页
+		//$grid->hideActions(); 隐藏操作
+		$grid->className('m-15');
+		$grid->column('id', '');
+		$grid->column('created_at', '');
+		$grid->column('updated_at', '');
+		$grid->column('name', '');
+		$grid->column('phone', '');
+		$grid->column('username', '');
+
+		return $grid;
+	}
+
+
+	protected function form($isEdit = false)
+	{
+		$form = new Form(new ModelZq);
+		$form->className('m-15');
+		$form->setEdit($isEdit);
+		$form->item('id', '');
+		$form->item('created_at', '');
+		$form->item('updated_at', '');
+		$form->item('name', '');
+		$form->item('phone', '');
+		$form->item('username', '');
+
+		return $form;
+	}
+}
---
+++ app/Model/Zq.php
@@ -0,0 +1,35 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model;
+
+use Hyperf\DbConnection\Model\Model;
+/**
+ * @property int $id 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @property string $name 
+ * @property string $phone 
+ * @property string $username 
+ */
+class Zq extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'zq';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+}
