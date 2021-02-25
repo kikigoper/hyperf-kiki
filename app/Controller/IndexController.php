b5b3fcaaffb1b4fa904684d3263e961fdaf7f9bf
kai <522402295@qq.com>
2021-02-25 16:32:12 +0800

    feat：缓存

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -13,30 +13,75 @@ namespace App\Controller;
 
 use App\Manager\HttpClient\GuzzleHttp;
 use Hyperf\HttpServer\Annotation\AutoController;
+use \App\Model\User;
+use Hyperf\Cache\Annotation\Cacheable;
+use Hyperf\Cache\Annotation\CacheEvict;
+use Hyperf\Cache\Annotation\CachePut;
 
 /**
  * @AutoController()
  */
 class IndexController extends AbstractController
 {
-    public function index()
+    //缓存(增加)
+    /**
+     * @Cacheable(prefix="user", ttl=7200, listener="USER_CACHE")
+     */
+    public function search()
     {
-//        $user = $this->request->input('foo', 'Hyperf');
-//        echo $user;
-//        $method = $this->request->getMethod();
-//        $model  = new GuzzleHttp();
-//        $data = $model->getJson('http://127.0.0.1:9701/index2/index',['foo' => 'get数据']);
-//        $user = $this->request->input('foo', 'Hyperf');
-//        echo $user;
-//        $method = $this->request->getMethod();
-//        $model  = new GuzzleHttp();
-//        http://127.0.0.1:9701/index2/index
-        $data = 1;
+        $user = User::query()->where('id',2)->first();
 
         return [
-//            'method' => $method,
-//            'message' => "Hello {$user}.",
-            'data' => $data,
+            'user' => $user->toArray(),
         ];
     }
+
+    //缓存(删)
+    /**
+     * @CacheEvict(prefix="user")
+     */
+//    public function index()
+//    {
+////        $user = User::query()->where('id',2)->first();
+//
+//        return true;
+////        return [
+////            'user' => $user->toArray(),
+////        ];
+//    }
+
+    /**
+     * @CachePut(prefix="user", ttl=3601)
+     */
+    public function update()
+    {
+        $user = User::query()->where('id',2)->first();
+        $user->name = 'HyperfDoc';
+        $user->save();
+
+        return [
+            'user' => $user->toArray(),
+        ];
+    }
+//    public function index():array
+//    {
+////        $user = $this->request->input('foo', 'Hyperf');
+////        echo $user;
+////        $method = $this->request->getMethod();
+////        $model  = new GuzzleHttp();
+////        $data = $model->getJson('http://127.0.0.1:9701/index2/index',['foo' => 'get数据']);
+////        $user = $this->request->input('foo', 'Hyperf');
+////        echo $user;
+////        $method = $this->request->getMethod();
+////        $model  = new GuzzleHttp();
+////        http://127.0.0.1:9701/index2/index
+//        $data = 35;
+//
+//        return [
+////            'method' => $method,
+////            'message' => "Hello {$user}.",
+//            'data' => $data,
+//        ];
+//    }
+
 }
---
+++ app/Model/User.php
@@ -0,0 +1,44 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model;
+
+use Hyperf\DbConnection\Model\Model;
+
+/**
+ * @property int $id 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @property string $name 
+ * @property string $phone 
+ * @property string $username 
+ */
+class User extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+
+    public $timestamps = false; // 默认created_at 和 updated_at
+
+    protected $casts = ['id' => 'integer', 'name' => 'string', 'phone' => 'string'];
+
+    public function test()
+    {
+        return 666;
+    }
+}
