831e86b442e8cc172dff174565db0fd0eb5c8cfb
522402295@qq.com <522402295>
2021-05-01 18:30:53 +0800

    feat:模块修改

--- app/Controller/Admin/AuthController.php
+++ app/Controller/Admin/AuthController.php
@@ -2,12 +2,7 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.plus
- *
- * @link     https://www.hyperf.plus
- * @document https://doc.hyperf.plus
- * @contact  4213509@qq.com
- * @license  https://github.com/hyperf-plus/admin/blob/master/LICENSE
+ * 入口模块
  */
 
 namespace App\Controller\Admin;
--- app/Controller/Admin/ZqController.php
+++ app/Controller/Admin/ZqController.php
@@ -3,12 +3,7 @@
 declare(strict_types=1);
 
 /**
- * This file is part of Hyperf.plus
- *
- * @link     https://www.hyperf.plus
- * @document https://doc.hyperf.plus
- * @contact  4213509@qq.com
- * @license  https://github.com/hyperf-plus/admin/blob/master/LICENSE
+ * 测试模块
  */
 
 namespace App\Controller\Admin;
